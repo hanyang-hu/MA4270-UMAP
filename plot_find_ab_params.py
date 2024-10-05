9a9efd1e5431eca1fabf97e9f7e59f3ae5de9834
Hanyang Hu <63213964+KoHomerHu@users.noreply.github.com>
2024-10-06 01:55:31 +0800

    Plot relations between forces and p_ij

--- plot_find_ab_params.py
+++ plot_find_ab_params.py
@@ -39,3 +39,29 @@ plt.xlabel('Distance')
 plt.ylabel('Grade of Membership')
 plt.show()
 
+# Plot attractive and repulsive forces for a set of p_ij
+def attractive_force(x, a, b, p):
+    return 2 * a * b * p / (x * (a + x ** (-2 * b)))
+
+def repulsive_force(x, a, b, p):
+    return 2 * b * (1-p) / (x * (1 + a * x ** (2 * b)))
+
+p_ij = [0.2, 0.5, 0.8]
+color = ['r', 'g', 'b']
+xv = np.linspace(0.00001, spread * 3, 300)
+plt.ylim(0, 5)
+for p in p_ij:
+    yv_at = attractive_force(xv, a, b, p)
+    yv_rp = repulsive_force(xv, a, b, p)
+    plt.plot(xv, yv_at, label=f'Attractive Force p_ij={p}', color=color[p_ij.index(p)], linestyle='-')
+    plt.plot(xv, yv_rp, label=f'Repulsive Force p_ij={p}', color=color[p_ij.index(p)], linestyle='--')
+    x_intersect = xv[np.argmin(np.abs(yv_at-yv_rp))]
+    y_intersect = yv_at[np.argmin(np.abs(yv_at-yv_rp))]
+    plt.scatter(x_intersect, y_intersect, c=color[p_ij.index(p)])
+plt.legend()
+plt.title('Attractive and Repulsive Forces')
+plt.xlabel('Distance')
+plt.ylabel('Absolute Value of Force')
+plt.show()
+
+
